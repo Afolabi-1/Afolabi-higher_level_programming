65e585f40e4854a69e7a6fcecc8cdf9835e79da2
afolabi <72478870+Afolabi-1@users.noreply.github.com>
2022-09-27 03:56:07 +0100

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python
 
 def divisible_by_2(my_list=[]):
-    even = []
-    for int in range(len(my_list)):
-        if my_list[int] % 2 == 0:
-            even.append(True)
-        else:
-            even.append(False)
-    return (even)
+    return [((var % 2) == 0) for var in my_list]
