49a25ea90d37ba29bb84f564b07789eedf57eecd
afolabi <72478870+Afolabi-1@users.noreply.github.com>
2022-09-27 03:24:36 +0100

    Create 8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+def multiple_returns(sentence):
+    """returns len of a str and first char."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
