50f36c4dd5aed4d7cc67ebe9b7a2857f9191e503
adetunji afolabi <adetunjiafolabi05@gmail.com>
2022-09-28 18:53:25 -0700

    Update

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    for x in set_1:
-        if x in set_2:
-            return (x)
+    """ Returns a common int between the two sets"""
+    return (set_1 & set_2)
