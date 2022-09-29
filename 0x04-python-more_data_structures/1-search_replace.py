bde550515c7f8daf0213b9e808033070c48fd06e
adetunji afolabi <adetunjiafolabi05@gmail.com>
2022-09-28 17:51:25 -0700

    Created 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    lst = my_list[:]
+    for x in range(len(lst_list)):
+        if lst[x] == search:
+            lst[x] = replace
+    return(lst) 
