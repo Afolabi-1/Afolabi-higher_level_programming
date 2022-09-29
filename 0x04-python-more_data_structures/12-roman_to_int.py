62bbf9dbcc232f928b7b624bad49a4a3fcfdf94e
afolabi <72478870+Afolabi-1@users.noreply.github.com>
2022-09-29 04:12:55 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -19,14 +19,14 @@ def roman_to_int(roman_string):
         "M": 1000
     }
     num = 0
+    r_dict = roman_dict
 
     for j in range(len(roman_string)):
-        if roman_dict.get(roman_string[j], 0) == 0:
+        if r_dict.get(roman_string[j], 0) == 0:
             return (0)
 
-        if (j != (len(roman_string) - 1) and
-            roman_dict[roman_string[j]] < roman_dict[roman_string[j + 1]]):
-                num += roman_dict[roman_string[j]] * -1
+        if (j != (len(roman_string) - 1) and r_dict[roman_string[j]] < r_dict[roman_string[j + 1]]):
+                num += r_dict[roman_string[j]] * -1
         else:
-            num += roman_dict[roman_string[j]]
+            num += r_dict[roman_string[j]]
     return (num)
