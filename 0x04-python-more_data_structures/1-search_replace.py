00f28218a1cf47abf3cf4b47a7bbef9f25b8046a
adetunji afolabi <adetunjiafolabi05@gmail.com>
2022-09-28 17:58:31 -0700

    Update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 
 def search_replace(my_list, search, replace):
     lst = my_list[:]
-    for x in range(len(lst_list)):
+    for x in range(len(my_list)):
         if lst[x] == search:
             lst[x] = replace
     return(lst) 
